4177d61eab3921bde26dc99da0032702fef1d7e2
Victor <victorfariasbezerra12@gmail.com>
2026-01-25 18:12:03 -0300

    Categories Context and Links

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -8,7 +9,10 @@ class Category(models.Model):
     image = models.ImageField(upload_to='photos/categories', blank=True, null=True)
 
     class Meta:
-        verbose_name_plural = "Categories"
+        verbose_name_plural = "categories"
+
+    def get_url(self):
+        return reverse('product_by_categories', args=[self.slug])
 
     def __str__(self):
         return self.name
