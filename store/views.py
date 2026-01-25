1309d4c9747c850f445b121e16fd89753523d5ca
Victor <victorfariasbezerra12@gmail.com>
2026-01-25 16:19:01 -0300

    Product by Category

--- store/views.py
+++ store/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
+def store(request, category_slug=None):
+    
+    if category_slug != None:
+        products = Product.objects.all().filter(is_available=True, category__slug=category_slug)
+    else:
+        products = Product.objects.all().filter(is_available=True)
+
     products_count = products.count()
 
     context = {'products': products, 'products_count': products_count}
